5827ac8d89a2d2e621154226fa17504ecc278733
gabriel soares <gabrielsantossoares18@gmail.com>
2023-03-24 22:06:53 -0300

    create exe e aulateste

---
+++ exercicio/ex004.py
@@ -0,0 +1,17 @@
+# n = float(input('Digite um número: '))
+
+# n = str(input('Digite um número: '))
+
+# n = bool(input('Digite um número: '))
+
+n = input('Digite algo: ')
+
+print(n .isupper())
+
+#print(n .isalnum())
+
+#print(n .isalpha())
+
+#print(n)
+
+#print(n .isnumeric())
---
+++ testepython/aula06a.py
@@ -0,0 +1,15 @@
+n1 = int(input('Digite um Numero: '))
+n2 = int(input('Digite outro Numero: '))
+n3 = int(input('Digite mas um Numero:'))
+s= n1 + n2 + n3
+
+#print ('A soma vale', s)
+#print ('A soma entre', n1, '+', n2, '+', n3 ,'é igual a', s)
+#print ('A soma entre {} + {} + {} é igual a {}' .format(n1,n2,n3,s))
+# print mas atualizado abaixo
+print (f'A soma de {n1} e {n2} e {n3} é {s}')
+
+#type mostra o tipe de valor dentro dos inputs tanto com string=str boolean=bool integer=int float=float
+#print(type(n1))
+#print(type(n2))
+#print(type(n3))
